5cdaad212ee89be35536cdb1e7fde980520af792
cks <kesheng.chen@molbase.com>
2018-01-20 09:12:43 +0800

    删除注释

--- src/main/java/com/roncoo/eshop/inventory/dao/RedisDAO.java
+++ src/main/java/com/roncoo/eshop/inventory/dao/RedisDAO.java
@@ -1,11 +1,7 @@
 package com.roncoo.eshop.inventory.dao;
 
 /**
- * redis本身有各种各样的api和功能
- * 
- * 可以做出来很多很多非常花哨的功能，但是我们这个课程，是有侧重点的，不是讲解redis基础知识的
- * 
- * 是讲解大规模缓存架构
+ * redis dao
  * 
  * @author Administrator
  *
